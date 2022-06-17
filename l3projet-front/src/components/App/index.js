99609d50ea757f6ea58815b3b9c641e274007390
Elanis <elanis@hotmail.com>
2022-06-17 11:11:42 +0200

    Game router

--- l3projet-front/src/components/App/index.js
+++ l3projet-front/src/components/App/index.js
@@ -1,3 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 export default function App() {
-	return <div>Test</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<div>Index</div>} />
+			<Route path="game">
+				<Route index element={<div>Game home</div>} />
+				<Route path="building/:buildingId" element={<div>Building view</div>} />
+			</Route>
+		</Routes>
+	);
 }
