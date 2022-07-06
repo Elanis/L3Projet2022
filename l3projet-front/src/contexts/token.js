a30c1f1f94ee8477442f6f36c615cf51b62276f3
Elanis <elanis@hotmail.com>
2022-07-06 09:05:48 +0200

    Persist token in localstorage

--- l3projet-front/src/contexts/token.js
+++ l3projet-front/src/contexts/token.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react';
 const TokenContext = createContext();
 
 export const TokenProvider = ({ children }) => {
-	const [token, setToken] = useState('');
+	const [token, setToken] = useState(localStorage.token || '');
 	return (
 		<TokenContext.Provider value={{ token, setToken }}>
 			{children}
@@ -14,5 +14,8 @@ export const TokenProvider = ({ children }) => {
 export default function useToken() {
 	const {token, setToken} = useContext(TokenContext);
 
-	return { token, isAuthenticated: token !== '', setToken };
+	return { token, isAuthenticated: token !== '', setToken: (token) => {
+		localStorage.token = token;
+		return setToken(token);
+	}};
 }
